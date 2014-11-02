296389097a37f094b11ef501dd229ff283974a5b
3280amanatidis <wolfmen04@hotmail.com>
2014-11-02 09:09:00 +0200

    DataChecks and ExceptionMessage update

--- src/ExceptionMessage.java
+++ src/ExceptionMessage.java
@@ -16,11 +16,15 @@ public class ExceptionMessage {
     
     
     public void ERRORMessage(){
-        
-        
+        JFrame window = new JFrame("ERROR Message");
+        int response1;
+        response1 = JOptionPane.showConfirmDialog(null, "Dothikan mi egira dedomena", "ERROR Message", 
+        JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);        
     }
     public void INFORMATIONMessage(){
-        
-        
+        JFrame window = new JFrame("INFORMATION Message");
+        int response1;
+        response1 = JOptionPane.showConfirmDialog(null, "Molis kanate Login", "INFORMATION Message", 
+        JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
     }
 }
