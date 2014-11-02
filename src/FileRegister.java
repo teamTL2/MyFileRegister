a4bab7fbfc4031267026d4bc99fa30d9ffb3258e
theodamia <theodamia@gmail.com>
2014-11-02 16:10:00 +0200

    User-File-Dev
    User - File creation, writing and reading.

--- src/FileRegister.java
+++ src/FileRegister.java
@@ -21,11 +21,60 @@ public class FileRegister extends FileCreator {
     {
         
     }
-
     
+    File usersFile = new File("C:\\Users\\wolf\\Documents\\NetBeansProjects\\MyFileRegister\\usersFile.txt");
+    boolean result = fileChecker(usersFile);
     
     public void fileWriter(Users user)
     {
+        try
+        {
+            if(result == true)
+            {
+                System.out.println("RESULT =  TRUE");
+                FileWriter fWriter = new FileWriter(usersFile.getAbsolutePath());
+                BufferedWriter bWriter = new BufferedWriter(fWriter);
+                bWriter.write(user.getName() + " " + user.getSurname() + " " + user.getPhone());
+                bWriter.newLine();
+                bWriter.close();
+            }
+            else
+            {
+                System.out.println("RESULT =  FALSE");
+                FileWriter fWriter = new FileWriter(usersFile.getAbsolutePath(), true);//bazoume edw True gia na mporoume na anoi3poume to arxeio se Append mode.
+                BufferedWriter bWriter = new BufferedWriter(fWriter);
+                bWriter.append(user.getName() + " " + user.getSurname() + " " + user.getPhone());
+                bWriter.newLine();
+                bWriter.close();
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error!!" +e);
+        }
         
+    }
+    
+    public void fileReader(Users user){
+        
+        try 
+        {
+            Scanner fileScanner = new Scanner(usersFile);
+            while(fileScanner.hasNextLine())
+            {
+                String line = fileScanner.nextLine();
+                
+                    if(line.contains(user.getSurname()) && line.contains(user.getPhone()))
+                    {
+                        ExceptionMessage message = new ExceptionMessage();
+                        message.INFORMATIONMessage();
+                    }
+                
+            }
+        } 
+        catch (FileNotFoundException ex) 
+        {
+            System.out.println("File not found!!" + ex);
+        }
     }
 }
